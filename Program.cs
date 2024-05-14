6cc9b5aebd953fe70571068d630e5ce901b4028d
SamuellHMP <samuelhmp@hotmail.com>
2024-05-14 11:26:29 -0300

    Desafio concluído.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "111111111", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Tinder");
+
+Console.WriteLine("");
+
+Console.WriteLine("iPhone:");
+Smartphone iphone = new Nokia("654321", "Modelo 2", "222222222", 256);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Tinder");
